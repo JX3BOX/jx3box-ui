0301ec889534ba2eb8345ea382745466c632e6d3
x3zvawq <x3zvawq@gmail.com>
2026-03-30 13:30:07 +0800

    fix(admin): 导入遗漏

--- src/bread/AdminDrop.vue
+++ src/bread/AdminDrop.vue
@@ -68,6 +68,7 @@ import CommunityAdmin from "./CommunityAdmin.vue";
 import { sendMessage } from "../../service/admin";
 import { refreshQQBotImage } from "../../service/cms";
 import i18nMixin from "../../i18n/mixin";
+import { editLink } from "@jx3box/jx3box-common/js/utils";
 export default {
     name: "AdminDrop",
     mixins: [i18nMixin],
